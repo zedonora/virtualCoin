44a5bd197c66b667043c4b3cdfc7423c0457c072
zedonora <timeundtime@gmail.com>
2019-07-20 13:41:56 +0900

    orderbook

--- test.js
+++ test.js
@@ -15,8 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // qs모듈로 쿼리스트�
 app.use(express.static("static"));
 
 let compareJson = [];
-let tradePercent = 0,
-  comparePercent = 0;
+let orderBookJson = [];
 
 app.get("/", (req, res) => {
   fs.readFile(__dirname + "/static/index.html", { json: true }, function(
@@ -92,6 +91,41 @@ app.get("/push", function(req, res) {
   res.sendFile(__dirname + "/static/push.html");
 });
 
+app.get("/order_book", function(req, res) {
+  fs.readFile(__dirname + "/static/orderBook.html", { json: true }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      res.writeHead(500);
+      return res.end("Error loading");
+    }
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(data);
+
+    const orderBookUrl = {
+      bithumb: "https://api.bithumb.com/public/orderbook/ETH",
+      upbit: "https://api.upbit.com/v1/orderbook?markets=KRW-ETH",
+      coinone: "https://api.coinone.co.kr/orderbook/",
+      poloniex:
+        "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_ETH&depth=1",
+      bitmex:
+        "https://www.bitmex.com/api/v1/orderBook/L2?symbol=ETHUSD&depth=1",
+      bittrex:
+        "https://api.bittrex.com/api/v1.1/public/getorderbook?market=USD-ETH&type=both",
+      kraken: "https://api.kraken.com/0/public/Depth?pair=XETHZUSD&count=1",
+      Korbit: "https://api.korbit.co.kr/v1/orderbook",
+      gopax: "https://api.gopax.co.kr/trading-pairs/ETH-KRW/book"
+    };
+    for (let key in orderBookUrl) {
+      callGetOrderBook(key, orderBookUrl[key]);
+    }
+    console.log("orderBookJson start>>>>>>>>>>>>>>>>>>>>>>>>>");
+    console.log(orderBookJson);
+    console.log("orderBookJson end>>>>>>>>>>>>>>>>>>>>>>>>>");
+  });
+});
+
 const promise1 = new Promise((resolve, reject) => {
   https
     .get("https://api.bithumb.com/public/ticker/ETH", res => {
@@ -165,6 +199,104 @@ const promise3 = new Promise((resolve, reject) => {
       console.log("error: " + err.message);
     });
 });
+
+const callGetOrderBook = (name, url) => {
+  console.log("callGetOrderBook>>>>>>" + name);
+  https
+    .get(url, res => {
+      let data = "";
+      let jsonData = {};
+      let tempJson = {};
+      res.on("data", chunk => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        jsonData = JSON.parse(data);
+        tempJson["id"] = name;
+        switch (name) {
+          //매도물량@최소매도호가, 매수물량@최대매수호가
+          case "bithumb":
+            tempJson["sell"] =
+              jsonData.data.bids[0].quantity +
+              "@" +
+              jsonData.data.bids[0].price;
+            tempJson["buy"] =
+              jsonData.data.asks[0].quantity +
+              "@" +
+              jsonData.data.asks[0].price;
+            break;
+
+          case "upbit":
+            tempJson["sell"] =
+              jsonData[0].orderbook_units[0].bid_size +
+              "@" +
+              jsonData[0].orderbook_units[0].bid_price;
+            tempJson["buy"] =
+              jsonData[0].orderbook_units[0].ask_size +
+              "@" +
+              jsonData[0].orderbook_units[0].ask_price;
+            break;
+
+          case "coinone":
+            tempJson["sell"] =
+              jsonData.bid[0].qty + "@" + jsonData.bid[0].price;
+            tempJson["buy"] = jsonData.ask[0].qty + "@" + jsonData.ask[0].price;
+            break;
+
+          case "poloniex":
+            tempJson["sell"] =
+              jsonData.bids[0][0][0] + "@" + jsonData.bids[0][0][1];
+            tempJson["buy"] =
+              jsonData.asks[0][0][0] + "@" + jsonData.asks[0][0][1];
+            break;
+
+          case "bitmex":
+            tempJson["sell"] = jsonData[0].size + "@" + jsonData[0].price;
+            tempJson["buy"] = jsonData[1].size + "@" + jsonData[1].price;
+            break;
+
+          case "bittrex":
+            tempJson["sell"] =
+              jsonData.result.sell[0].Quantity +
+              "@" +
+              jsonData.result.sell[0].Rate;
+            tempJson["buy"] =
+              jsonData.result.buy[0].Quantity +
+              "@" +
+              jsonData.result.buy[0].Rate;
+            break;
+
+          case "kraken":
+            tempJson["sell"] =
+              jsonData.result.XETHZUSD.bids[0][1] +
+              "@" +
+              jsonData.result.XETHZUSD.bids[0][0];
+            tempJson["buy"] =
+              jsonData.result.XETHZUSD.asks[0][1] +
+              "@" +
+              jsonData.result.XETHZUSD.asks[0][0];
+            break;
+
+          case "Korbit":
+            tempJson["sell"] =
+              jsonData.bids[0][0][1] + "@" + jsonData.bids[0][0][0];
+            tempJson["buy"] =
+              jsonData.asks[0][0][1] + "@" + jsonData.asks[0][0][0];
+            break;
+
+          case "gopax":
+            tempJson["sell"] = jsonData.bid[0][2] + "@" + jsonData.bid[0][1];
+            tempJson["buy"] = jsonData.ask[0][2] + "@" + jsonData.ask[0][1];
+            break;
+        }
+
+        orderBookJson.push(tempJson);
+      });
+    })
+    .on("error", err => {
+      console.log("error: " + err.message);
+    });
+};
 
 const callPost = (sendData, hostname, path, type) => {
   new Promise((resolve, reject) => {
